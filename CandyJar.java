1dea104118df3269017adb8d5dd9b4ad93b080f6
Katie Hou <katiehkotlc@gmail.com>
2026-08-26 19:04:29 +0000

    CandyJar done

--- CandyJar.java
+++ CandyJar.java
@@ -1,3 +1,19 @@
 public class CandyJar {
-    
+    private int amount;
+
+    public CandyJar() {
+        amount = 10;
+    }
+
+    public CandyJar(int a) {
+        amount = a; //right to left for a reason, a = amount is not the same thing
+    }
+
+    public void eat(int amt) {
+        amount =- amt;
+    }
+
+    public void refill(int amt) {
+        amount =+ amt;
+    }
 }
