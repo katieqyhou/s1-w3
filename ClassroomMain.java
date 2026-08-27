d71656154e4e1d5f617f0bd988ea3ec1ed3a906c
Katie Hou <katiehkotlc@gmail.com>
2026-08-27 06:47:46 +0000

    halfway

--- ClassroomMain.java
+++ ClassroomMain.java
@@ -1,5 +1,12 @@
 public class ClassroomMain {
     public static void main(String[] args) {
-        
+        Classroom c = new Classroom("Morris", 20, 15);
+        Classroom c1 = new Classroom("Castle");
+
+        c1.setDesks(30);
+        System.out.println(c.teacher());
+        System.out.println(c.desks());
+        System.out.println(c.students());
+
     }
 }
